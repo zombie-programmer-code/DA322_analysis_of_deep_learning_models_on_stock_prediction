1ee1f64d113561e5def5ea06a4f7c7420aa5b715
Saptarshi Mukherjee <121932403+zombie-programmer-code@users.noreply.github.com>
2024-11-27 20:03:20 +0530

    Update regular_lstm.py

--- models/regular_lstm_models/regular_lstm.py
+++ models/regular_lstm_models/regular_lstm.py
@@ -1,3 +1,4 @@
+#Models suitable for predicting given 10, 30, 60 previous days data. There are 4 models for each type-the average of these 4 gives the prediction. 
 def lstm_stock_model(X_train, y_train, X_dev, y_dev, epochs=2, batch_size=64, first_layer=60):
     model = Sequential()
     model.add(LSTM(units=first_layer, activation = 'relu', return_sequences=True, input_shape=(X_train.shape[1], 1)))
